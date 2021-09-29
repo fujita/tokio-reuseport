92e46ed4552493da256a812893de9e6173293fb6
FUJITA Tomonori <fujita.tomonori@gmail.com>
2021-09-29 06:41:38 +0000

    add greeter-basic
    textbook code.
    
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@gmail.com>

---
+++ greeter-basic/src/main.rs
@@ -0,0 +1,58 @@
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
+
+use tonic::{transport::Server, Request, Response, Status};
+
+use hello_world::greeter_server::{Greeter, GreeterServer};
+use hello_world::{HelloReply, HelloRequest};
+
+pub mod hello_world {
+    tonic::include_proto!("helloworld");
+}
+
+#[derive(Debug, Default)]
+pub struct MyGreeter {}
+
+#[tonic::async_trait]
+impl Greeter for MyGreeter {
+    async fn say_hello(
+        &self,
+        request: Request<HelloRequest>,
+    ) -> Result<Response<HelloReply>, Status> {
+        let reply = hello_world::HelloReply {
+            message: format!("Hello {}!", request.into_inner().name),
+        };
+        Ok(Response::new(reply))
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let greeter = MyGreeter::default();
+
+    let addr: std::net::SocketAddr = "0.0.0.0:50051".parse().unwrap();
+    let sock = socket2::Socket::new(
+        match addr {
+            SocketAddr::V4(_) => socket2::Domain::IPV4,
+            SocketAddr::V6(_) => socket2::Domain::IPV6,
+        },
+        socket2::Type::STREAM,
+        None,
+    )
+    .unwrap();
+
+    sock.set_reuse_address(true).unwrap();
+    sock.set_reuse_port(true).unwrap();
+    sock.set_nonblocking(true).unwrap();
+    sock.bind(&addr.into()).unwrap();
+    sock.listen(16384).unwrap();
+
+    let incoming =
+        tokio_stream::wrappers::TcpListenerStream::new(TcpListener::from_std(sock.into()).unwrap());
+
+    Server::builder()
+        .add_service(GreeterServer::new(greeter))
+        .serve_with_incoming(incoming)
+        .await
+        .unwrap();
+}
